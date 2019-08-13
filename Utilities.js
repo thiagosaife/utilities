001b984971fa3ac35bfad36364c86b8f7d9a1cc4
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2019-08-13 10:36:00 -0300

    Sorting nested objects

--- Utilities.js
+++ Utilities.js
@@ -1,9 +1,22 @@
 class Utilities {
   sortArray(array, field) {
-    if (field) {
+    if (Array.isArray(fields)) {
+      array.sort((a, b) => {
+        let i = 0;
+        while (i < fields.length) {
+          a = a[fields[i]];
+          b = b[fields[i]];
+          i += 1;
+        }
+        if (a > b) return 1;
+        if (b > a) return -1;
+        return 0;
+      });
+    }
+    if (!Array.isArray(fields)) {
       return array.sort((a, b) => {
-        if (a[field] < b[field]) return -1;
-        if (a[field] > b[field]) return 1;
+        if (a[fields] < b[fields]) return -1;
+        if (a[fields] > b[fields]) return 1;
         return 0;
       });
     }
