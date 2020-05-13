de6c75fd6a6ce85a791e124de381f75a47872e30
Thiago Saife Rodrigues <thiago.rodrigues@THIAGORODRIGUES.local>
2020-05-13 19:28:22 -0300

    Changing initialization value

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,7 @@
 const A = [5, 2, 4, 6, 1, 3, 8, 1, 10, 12, 13, 4, 7];
 
 const insertionSort = (array) => {
-  for (let j = 1; j < array.length; j++) {
+  for (let j = 2; j < array.length; j++) {
     let i = j - 1;
     let key = array[j];
     while (i >= 0 && array[i] > key) {
